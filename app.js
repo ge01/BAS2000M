0bfcda5a908fdc10a70b75a334144840ca98df5a
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-21 12:10:21 -0500

    added test data and comments

--- app.js
+++ app.js
@@ -4,16 +4,26 @@
 * the letters of the string in the second element of the array.                 *
 ********************************************************************************/
 function mutation(arr) {
+  // Ensure characters in arr are lower case
   var lowerArr1 = arr[1].toLowerCase();
   var lowerArr0 = arr[0].toLowerCase();
+  // Loop through length of the string lowerArr1
   for (i=0;i<lowerArr1.length;i++) {
+    // if the given element can't be found -1 will be compared to 0
+    // and return false
     if (lowerArr0.indexOf(lowerArr1[i]) < 0)
       return false;
   }
   return true;
  }
 
+// Input Test Data
 console.log(mutation(["hello", "hey"]));
 console.log(mutation(["Mary", "Aarmy"]));
 console.log(mutation(["Alien", "line"]));
 console.log(mutation(["floor", "for"]));
+console.log(mutation(["hello", "Hello"]));
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+console.log(mutation(["Mary", "Army"]));
+console.log(mutation(["hello", "neo"]));
+console.log(mutation(["voodoo", "no"]));
