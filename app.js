372ca41a19f9ad44f8125861c8d57526894dd6df
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-21 02:00:09 -0500

    doesn't work on floor

--- app.js
+++ app.js
@@ -3,19 +3,52 @@
 * Return true if the string in the first element of the array contains all of   *
 * the letters of the string in the second element of the array.                 *
 ********************************************************************************/
-//console.log("hello".indexOf("hey"));
-//console.log("hello".indexOf("Hello"));
+function mutation(arr) {
+  console.log(arr[0]);
+  var filter = new RegExp("[" + arr[1] + "]", "gi");
+  console.log(filter);
+  var whatMatchedArr = arr[0].match(filter);
+  //var stringToArr = arr[0].split('');
+  console.log(whatMatchedArr);
 
-var a = ["hello", "hey"];
-//console.log(a[0].indexOf(a[1]));
+  var whatMatchedStr = whatMatchedArr.join('');
+  //var cB = arr[0].sort.join(',');
+  console.log(whatMatchedStr);
+  //console.log(cB);
+  //console.log(whatMatchedArr.length);
+  if (whatMatchedStr === arr[0] || whatMatchedArr.length === arr[1].length){
+    return true;
+  } else {
+    return false;
+  }
 
-var b = ["Mary", "ARMY"];
+
+  //console.log(stringToArr);
+  //console.log(whatMatchedArr.indexOf(stringToArr));
+  //return whatMatchedArr.equals(stringToArr);
+}
+
+//console.log(mutation(["Mary", "Aarmy"]));
+//console.log(mutation(["Alien", "line"]));
+console.log(mutation(["floor", "for"]));
+
+//var b = ["Mary", "ARMY"];
 // console.log(b[0].match(/[b[1]]/gi));
 //strTemp = b[1];
 //console.log(strTemp);
 //console.log(b[0].match(/[strTemp]/gi));
 
-var filter = new RegExp("[" + b[1] + "]", "gi");
-console.log(filter);
-console.log(b[0].match(filter));
-console.log(b[0].match(/[ARMY]/gi));
+//var filter = new RegExp("[" + b[1] + "]", "gi");
+//console.log(filter);
+//console.log(b[0].match(filter));
+//var whatMatchedArr = b[0].match(filter);
+//console.log(whatMatchedArr);
+//console.log(whatMatchedArr.length);
+/*
+if (whatMatchedArr.length === b[1].length){
+  console.log(true);
+}
+else {
+  console.log(false);
+}
+*/
